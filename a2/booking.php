076d160b2ff58267c7078b95e50418513b0e899c
EvertVisser <48049866+s3727884@users.noreply.github.com>
2022-01-03 10:46:54 +1100

    Updated booking page with basic links
    Basic link and style update.

--- a2/booking.php
+++ a2/booking.php
@@ -13,24 +13,31 @@
 
   <body>
 
-    <header>
-      <div>Put company logo and name here</div>
-    </header>
-
-    <nav>
-      <div>Put website navigation links here</div>
-    </nav>
+    <section class="header">
+        <nav> 
+          <a href="index.html"><img src="images/logo2.png"></a>
+          <div class="navLinks">
+            <ul>
+              <li><a href="index.html">HOME</a></li>
+              <li><a href="booking.html">BOOKINGS</a></li>
+              <li><a href="#">ABOUT</a></li>
+              <li><a href="#nowShowing">NOW SHOWING</a></li>
+              <li><a href="#seatsPricing">SEATS & PRICING</a></li>
+              <li><a href="#contactInfo">CONTACT US</a></li>
+            </ul>
+          </div>
+          
+       </section>
 
     <main>
-      <article id='Website Under Construction'>
-    <!-- Creative Commons image sourced from https://pixabay.com/en/maintenance-under-construction-2422173/ and used for educational purposes only -->
-        <img src='../../media/website-under-construction.png' alt='Website Under Construction' />
-      </article>
+
+      
     </main>
     <footer>
       <div>&copy;<script>
         document.write(new Date().getFullYear());
-      </script> Put your name(s), student number(s) and group name here. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
+      </script> Evert Visser, S3727884, Group 1. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
+      <a href="https://github.com/s3727884/wp.git" target="_blank">GitHub Link</a> 
       <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
       <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
     </footer>
