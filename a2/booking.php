058586e481b6e285ab8032e4f76fa5933a1d1544
EvertVisser <48049866+s3727884@users.noreply.github.com>
2022-01-03 11:44:36 +1100

    Minor coding update
    Redirection to image source updated.

--- a2/booking.php
+++ a2/booking.php
@@ -15,7 +15,7 @@
 
     <section class="header">
         <nav> 
-          <a href="index.php"><img src="images/logo2.png"></a>
+          <a href="index.php"><img src="../media/logo2.png"></a>
           <div class="navLinks">
             <ul>
               <li><a href="index.php">HOME</a></li>
