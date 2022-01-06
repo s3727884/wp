f1de4efdd1417079f921e2594d34fdc120dd3882
EvertVisser <48049866+s3727884@users.noreply.github.com>
2022-01-06 21:23:01 +1100

    Bulk Code Change
    re-Write of full code due to me not liking.

--- a2/booking.php
+++ a2/booking.php
@@ -9,38 +9,80 @@
     <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
     <link id='stylecss' type="text/css" rel="stylesheet" href="style.css?t=<?= filemtime("style.css"); ?>">
     <script src='../wireframe.js'></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+
+    
   </head>
 
   <body>
 
+    <!--// LOGO and NAV -->
     <section class="header">
-        <nav> 
-          <a href="index.php"><img src="../../media/Logo2.png"></a>
-          <div class="navLinks">
-            <ul>
-              <li><a href="index.php">HOME</a></li>
-              <li><a href="booking.php">BOOKINGS</a></li>
-              <li><a href="#">ABOUT</a></li>
-              <li><a href="#nowShowing">NOW SHOWING</a></li>
-              <li><a href="#seatsPricing">SEATS & PRICING</a></li>
-              <li><a href="#contactInfo">CONTACT US</a></li>
-            </ul>
-          </div>
-          
-       </section>
+      <nav>
+          <a href ="index.html"><img src="images/Logo2.png" class="logo"></a>
+          <div class="navbar">
+          <ul>
+              <li><a href="index.html">HOME</a></li>
+              <li><a href="index.html#about">ABOUT</a></li>
+              <li><a href="booking.html">BOOKINGS</a></li>
+              <li><a href="index.html#nowshowing">NOW SHOWING</a></li>
+              <li><a href="index.html#seatspricing">SEATS & PRICING</a></li>
+              <li><a href="#contact">CONTACT</a></li>
+          </ul>
+      </div>
+      </nav>
 
-    <main>
+      <div class="text-box">
+        <h1 id="booking">BOOKINGS</h1>
+        <p>This is the bookings page.</p>
+        <p>get yer tickets here.</p>
 
-      
-    </main>
+    </div>
+
+    </section>
+   
+
+
+
     <footer>
-      <div>&copy;<script>
-        document.write(new Date().getFullYear());
-      </script> Evert Visser, S3727884, Group 1. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
-      <a href="https://github.com/s3727884/wp.git" target="_blank">GitHub Link</a> 
-      <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
-      <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
-    </footer>
+    <div class="row">
+      <div class="col">
+          <p>GITHUB LINK</p>
+        <a href="https://github.com/s3727884/wp.git" target="blank"><img src="images/GitHubLogo.png" class="gitlogo"></a>
+          <p>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</p>
+      </div>
+      <div class="col">
+          <h3 id="contact">Contact</h3>
+          <p>1500 That Drive</p>
+          <p>No-the-other-Steet, TheMoon</p>
+          <p>FirstCrater, SecondFootstep, Luna</p>
+          <p class="email-id">thelunardocinema@cinema.com.moon</p>
+          <h4>Phone: 1300 goodluck calling</h4>
+      </div>
+      <div class="col">
+          <h3>Links</h3>
+          <ul>
+            <li><a href="index.html">HOME</a></li>
+            <li><a href="index.html#about">ABOUT</a></li>
+            <li><a href="booking.html">BOOKINGS</a></li>
+            <li><a href="index.html#nowshowing">NOW SHOWING</a></li>
+            <li><a href="index.html#seatspricing">SEATS & PRICING</a></li>
+          </ul>
+      </div>
+
+      <div class="col">
+          <h3>Required Info</h3>
+          
+          <div>&copy;<script>
+            document.write(new Date().getFullYear());
+          </script> Evert Visser, S3727884, Group 1. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
+          <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
+      </div>
+   </div>
+  <hr>
+  <p class="copyright">Evert Visser © 2022 - All Rights Reserved</p>
+</footer>
+
     <aside id="debug">
       <hr>
       <h3>Debug Area</h3>
@@ -55,4 +97,6 @@ SESSION Contains:
     </aside>
 
   </body>
+
+
 </html>
