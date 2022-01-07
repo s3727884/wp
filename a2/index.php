068c96441eb4076764c5bf342bd915c8918d5aaf
EvertVisser <48049866+s3727884@users.noreply.github.com>
2022-01-07 18:13:08 +1100

    NavBar script insertion
    Inserting script for sticky black nav bar

--- a2/index.php
+++ a2/index.php
@@ -214,6 +214,19 @@
       <hr>
       <p class="copyright">Evert Visser © 2022 - All Rights Reserved</p>
     </footer>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" ></script>
+
+    <script type="text/javascript">
+
+        $(window).on("scroll", function(){
+            if($(window).scrollTop()) {
+                $("nav").addClass("black");
+            }
+            else{
+                $("nav").removeClass("black");
+            }
+        })
+    </script>
 
 
 </body>
