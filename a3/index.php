220faf7381200f28207bcb9cf6c5a2904e3003e8
EvertVisser <48049866+s3727884@users.noreply.github.com>
2022-02-18 13:57:09 +1100

    Debug area inclusion
    Added debug area to page

--- a3/index.php
+++ a3/index.php
@@ -307,6 +307,18 @@
         }
       });
     </script>
+        <aside id="debug">
+      <hr>
+      <h3>Debug Area</h3>
+      <pre>
+GET Contains:
+<?php print_r($_GET) ?>
+POST Contains:
+<?php print_r($_POST) ?>
+SESSION Contains:
+<?php print_r($_SESSION) ?>
+      </pre>
+    </aside>
   </body>
 </html>
 
