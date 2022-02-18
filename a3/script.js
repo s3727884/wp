8c90339ab9d42f611518dc6da3aab3d0b10b8440
EvertVisser <48049866+s3727884@users.noreply.github.com>
2022-02-18 18:40:41 +1100

    Script update
    Script handeling update

--- a3/script.js
+++ a3/script.js
@@ -1,5 +1,6 @@
 const sections = document.querySelectorAll("section");
 const navLi = document.querySelectorAll("nav .navbar ul li");
+
 window.addEventListener("scroll", () => {
   let current = "";
   sections.forEach((section) => {
@@ -23,64 +24,63 @@ const username = document.getElementById("username");
 const phone = document.getElementById("phone");
 const email = document.getElementById("email");
 
-form.addEventListener("bookbtn", (e) => {
+form.addEventListener("click", (e) => {
   e.preventDefault();
 
   checkInputs();
+
+  function checkInputs() {
+    //get values from inputs
+    const usernameValue = username.value.trim();
+    const phoneValue = phone.value.trim();
+    const emailValue = email.value.trim();
+
+    if (usernameValue === " ") {
+      setErrorFor(username, "Username cannot be blank");
+    } else {
+      setSuccessFor(username);
+    }
+
+    if (phoneValue === " ") {
+      setErrorFor(phone, "Phone cannot be blank");
+    } else if (!isPhone(phoneValue)) {
+      setErrorFor(phone, "Not a valid phone number");
+    } else {
+      setSuccessFor(phone);
+    }
+
+    if (emailValue === " ") {
+      setErrorFor(email, "Email cannot be blank");
+    } else if (!isEmail(emailValue)) {
+      setErrorFor(email, "Not a valid email");
+    } else {
+      setSuccessFor(email);
+    }
+  }
+
+  function setErrorFor(input, message) {
+    const personalInfo = input.parentElement;
+    const small = personalInfo.querySelector("small");
+
+    //add error message inside small
+    small.innerText = message;
+
+    // add error class
+    personalInfo.className = "personalInfo error";
+  }
+
+  function setSuccessFor(input) {
+    const personalInfo = input.parentElement;
+    personalInfo.className = "personalInfo success";
+  }
+
+  function isEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+      email
+    );
+  }
+
+  function isPhone(phone) {
+    return /^\d{7,}$/.test(value.replace(/[\s()+\-\.]|ext/gi, "")).test(phone);
+  }
 });
-
-function checkInputs() {
-  //get values from inputs
-  const usernameValue = username.value.trim();
-  const phoneValue = phone.value.trim();
-  const emailValue = email.value.trim();
-
-  if (usernameValue === "") {
-    setErrorFor(username, "Username cannot be blank");
-  } else {
-    setSuccessFor(username);
-  }
-
-  if (phoneValue === "") {
-    setErrorFor(phone, "Phone cannot be blank");
-  } else if (!isPhone(phoneValue)) {
-    setErrorFor(phone, "Not a valid phone number");
-  } else {
-    setSuccessFor(phone);
-  }
-
-  if (emailValue === "") {
-    setErrorFor(email, "Email cannot be blank");
-  } else if (!isEmail(emailValue)) {
-    setErrorFor(email, "Not a valid email");
-  } else {
-    setSuccessFor(email);
-  }
-}
-
-function setErrorFor(input, message) {
-  const formControl = input.parentElement;
-  const small = formControl.querySelector("small");
-
-  //add error message inside small
-  small.inerText = message;
-
-  // add error class
-  formControl.className = "form-control error";
-}
-
-function setSuccessFor(input) {
-  const formControl = input.parentElement;
-  formControl.className = "form-control success";
-}
-
-function isEmail(email) {
-  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-    email
-  );
-}
-
-function isPhone(phone) {
-  return /^\d{7,}$/.test(value.replace(/[\s()+\-\.]|ext/gi, "")).test(phone);
-}
-
