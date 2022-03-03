4477af21fd5924ea3e10e9ee347d1d42a251e523
EvertVisser <48049866+s3727884@users.noreply.github.com>
2022-03-04 07:52:18 +1100

    Heading update
    Heading update to A4

--- a4/index.php
+++ a4/index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Assignment 3</title>
+    <title>Assignment 4 - Exam</title>
 
     <!-- Keep wireframe.css for debugging, add your css to style.css -->
     <link
